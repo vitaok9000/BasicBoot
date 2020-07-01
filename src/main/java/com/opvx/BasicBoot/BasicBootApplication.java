d3e4264e1b2de56ed0a15a7d1cb0ab8d650a61ba
unknown <vitaok9000@gmail.com>
2020-07-01 01:02:27 -0300

    init arquitetura

---
+++ src/main/java/com/opvx/BasicBoot/BasicBootApplication.java
@@ -0,0 +1,13 @@
+package com.opvx.BasicBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opvx/BasicBoot/BasicBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opvx.BasicBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
