2585cfb9f44e2fa7fcef6e78bfac5694dece0e53
Vitor Silva <kstack.vsilva@hubfintech.com.br>
2020-07-22 10:18:15 -0300

    hello world

--- src/main/java/com/opvx/BasicBoot/BasicBootApplication.java
+++ src/main/java/com/opvx/BasicBoot/BasicBootApplication.java
@@ -2,8 +2,10 @@ package com.opvx.BasicBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class BasicBootApplication {
 
 	public static void main(String[] args) {
